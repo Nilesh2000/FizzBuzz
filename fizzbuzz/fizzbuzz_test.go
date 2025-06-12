f9dce52e34566040c1733127ee52e489c729b05c
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-06-12 16:14:52 +0530

    [ABC-123] table driven tests

--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -76,3 +76,22 @@ func TestFizzBuzzNEquals15(t *testing.T) {
 		t.Errorf("FizzBuzz(%d) = %v, want %v", n, got, want)
 	}
 }
+
+func TestFizzBuzz(t *testing.T) {
+	tests := []struct {
+		n    int
+		want []string
+	}{
+		{1, []string{"1"}},
+		{3, []string{"1", "2", "Fizz"}},
+		{5, []string{"1", "2", "Fizz", "4", "Buzz"}},
+		{15, []string{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"}},
+	}
+
+	for _, test := range tests {
+		got := FizzBuzz(test.n)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("FizzBuzz(%d) = %v, want %v", test.n, got, test.want)
+		}
+	}
+}
