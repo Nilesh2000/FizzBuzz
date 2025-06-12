079b54e16db07515df8cae0d89269d33972b9d2b
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-06-12 15:59:39 +0530

    refactor: add constants

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,15 +1,21 @@
 package fizzbuzz
 
+const (
+	FIZZ     = "Fizz"
+	BUZZ     = "Buzz"
+	FIZZBUZZ = "FizzBuzz"
+)
+
 func FizzBuzz(n int) []string {
 	switch n {
 	case 1:
 		return []string{"1"}
 	case 3:
-		return []string{"1", "2", "Fizz"}
+		return []string{"1", "2", FIZZ}
 	case 5:
-		return []string{"1", "2", "Fizz", "4", "Buzz"}
+		return []string{"1", "2", FIZZ, "4", BUZZ}
 	case 15:
-		return []string{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"}
+		return []string{"1", "2", FIZZ, "4", BUZZ, FIZZ, "7", "8", FIZZ, BUZZ, "11", FIZZ, "13", "14", FIZZBUZZ}
 	default:
 		return []string{}
 	}
